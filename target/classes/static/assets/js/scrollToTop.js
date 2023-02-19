af8793282cdb718bac42cb459de08bf359e40d36
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2023-02-19 15:49:15 -0600

    Add functionality to scrollbutton

--- target/classes/static/assets/js/scrollToTop.js
+++ target/classes/static/assets/js/scrollToTop.js
@@ -1,12 +1,21 @@
-
-window.addEventListener('DOMContentLoaded', ()=>{
-    function scrollToTop() {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+function buttonVisibility(scrollBtn){
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
     }
+}
+window.addEventListener('DOMContentLoaded', ()=>{
 
     const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    window.onscroll = function (){
+        buttonVisibility(scrollToTopBtn)
+    }
     scrollToTopBtn.addEventListener("click", scrollToTop);
 })
