7ca18b2d642cb06d9662e58929f3ee3e25864c5d
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2023-02-19 14:24:42 -0600

    Add home Controller

--- src/main/java/com/jenkins/capstone/controller/HomeController.java
+++ src/main/java/com/jenkins/capstone/controller/HomeController.java
@@ -2,11 +2,17 @@ package com.jenkins.capstone.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+    /**
+     * Displays the home view
+     * @return
+     */
+    @RequestMapping(value = {"", "/", "home"})
     public String displayHomeView(){
-        return "Hello world";
+        return "home.html";
     }
 }
