c36f2fbdec74943531b22dde0ec51903f2328e95
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2023-02-19 18:36:09 -0600

    Add static projects view

--- src/main/java/com/jenkins/capstone/controller/HomeController.java
+++ src/main/java/com/jenkins/capstone/controller/HomeController.java
@@ -16,6 +16,6 @@ public class HomeController {
      */
     @RequestMapping(value = {"", "/", "home"})
     public String displayHomeView(){
-        return "about";
+        return "projects";
     }
 }
