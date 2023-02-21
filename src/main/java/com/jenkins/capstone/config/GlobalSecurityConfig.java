7fb8b5522d75f4ff59d4335045c5525ef08b28e8
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2023-02-21 15:06:05 -0600

    Add Security package, Add AuthenticationProvider class

--- src/main/java/com/jenkins/capstone/config/GlobalSecurityConfig.java
+++ src/main/java/com/jenkins/capstone/config/GlobalSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.jenkins.capstone.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@ComponentScan("com.jenkins.capstone.security")
 public class GlobalSecurityConfig {
 
 
