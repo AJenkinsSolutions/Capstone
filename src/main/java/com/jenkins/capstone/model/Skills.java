835d9d532826e3bf64fa7e4e176c972befc5df1b
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2023-02-21 09:57:32 -0600

    Add developerRepo

--- src/main/java/com/jenkins/capstone/model/Skills.java
+++ src/main/java/com/jenkins/capstone/model/Skills.java
@@ -1,10 +1,7 @@
 package com.jenkins.capstone.model;
 
 import com.jenkins.capstone.audit.AuditEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
@@ -22,9 +19,11 @@ public class Skills extends AuditEntity {
     private String language;
 
     @NotBlank(message = "Specify if front End development is in your skill set")
+    @Column(name = "front_end")
     private boolean frontEnd;
 
     @NotBlank(message = "Specify if backEnd development is in your skill set")
+    @Column(name = "back_end")
     private boolean backEnd;
 
     //This field is not required
--- src/main/java/com/jenkins/capstone/repository/RoleRepository.java
+++ src/main/java/com/jenkins/capstone/repository/RoleRepository.java
@@ -4,6 +4,9 @@ import com.jenkins.capstone.model.Roles;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Jpa Give us access to the sorting and pagination
+ */
 @Repository
 public interface RoleRepository extends JpaRepository <Roles, Integer> {
 }
